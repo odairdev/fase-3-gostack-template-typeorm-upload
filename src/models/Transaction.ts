71ce54fb004162ea72ba3192878165cba197ffe7
odairdev <odair.dev@gmail.com>
2021-01-29 22:42:24 -0300

    Migrations and Entities added, migrations already ran.

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,12 +1,35 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  OneToMany,
+  JoinColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne
+} from 'typeorm'
+import Category from './Category';
+
+@Entity('transactions')
 class Transaction {
+
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
+  @Column()
   type: 'income' | 'outcome';
 
+  @Column()
   value: number;
 
+  @ManyToOne(() => Category, category => category.transaction, {eager: true})
+  @JoinColumn({name: 'category_id'})
+  category: string
+
+  @OneToMany(() => Transaction, transaction => transaction.category)
   category_id: string;
 
   created_at: Date;
