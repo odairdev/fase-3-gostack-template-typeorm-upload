0d684d767ffb52eae18cc0cf69e38490662921a0
odairdev <odair.dev@gmail.com>
2021-02-02 09:13:27 -0300

    Config pushed

---
+++ src/database/migrations/1611939841724-CreateTransactions.ts
@@ -0,0 +1,54 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateTransactions1611939841724 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'transactions',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'uuid',
+                        isPrimary: true,
+                        generationStrategy: "uuid",
+                        default: "uuid_generate_v4()"
+                    },
+                    {
+                        name: 'title',
+                        type: 'varchar'
+                    },
+                    {
+                        name: 'type',
+                        type: 'varchar'
+                    },
+                    {
+                        name: 'value',
+                        type: 'int'
+                    },
+                    {
+                        name: 'category_id',
+                        type: 'uuid',
+                        isNullable: true
+                    },
+                    {
+                        name: 'created_at',
+                        type: 'timestamp',
+                        default: 'now()'
+                    },
+                    {
+                        name: 'updated_at',
+                        type: 'timestamp',
+                        default: 'now()'
+                    }
+
+                ]
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {
+        await queryRunner.dropTable('transactions')
+    }
+
+}
